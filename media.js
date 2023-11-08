240fb72135e627b0c0a0a37e06f33f520dc27f91
katlyn19 <113352442+katlyn19@users.noreply.github.com>
2023-11-08 09:01:51 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -6,11 +6,13 @@ const resp2 = document.querySelector("h4")
 
 //Cria um ouvinte
 
- form.addEventListener("submit",(e) =>){e.preventDefault()       //evita o envio do form 
+ form.addEventListener("submit",(e) =>{
+    e.preventDefault()       //evita o envio do form 
+ 
  const nome = form.inNome.value                 //obtém o nome
  const nota1 = Number(form.inNota1.value)       //obtém a primeira nota
  const nota2 = Number(form.inNota2.value)       //obtém a segunda nota
- const media = (nota1 + nota2.value)            //média das notas 
+ const media = (nota1 + nota2)/2        //média das notas 
  resp1.innerText = `Média das Notas ${media.toFixed(2)}` 
  
 //Cria as condicionais
@@ -19,6 +21,10 @@ if(n=media >= 7) {
             resp2.innerText =`Parabéns ${nome}! Você foi aprovado(a)`
             resp2.style.color = "blue"
 } else if (media >=4) {
-    resp2.innerText = `Atenção`
+    resp2.innerText = `Atenção ${nome}! Voce esta de exame `
+resp2.style.color = "green"
+}else{
+    resp2.innerText = `Ops $(nome)! Voce foi reprovado(a)`
+    resp2.style.color = "red"
 }
- 
+})
